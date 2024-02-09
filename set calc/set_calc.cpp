6e9ec5a6c4078734e9db1000b263f76a02606a9a
Simon946 <56819218+Simon946@users.noreply.github.com>
2024-02-09 11:18:37 +0100

    Update set_calc.cpp
    fix

--- set calc/set_calc.cpp
+++ set calc/set_calc.cpp
@@ -27,7 +27,7 @@ set<string> inputSet();
 class SetExpr{
     public:
         SetExpr(SetExpr* lhs, ExprType operand, SetExpr* rhs);
-        SetExpr(set<string> value);
+        SetExpr(set<string>& value);
         ~SetExpr();
         SetExpr* calculate();
         string toString();
